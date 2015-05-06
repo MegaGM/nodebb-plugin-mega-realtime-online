9a40d53af4afaf9d66852c6f663fc4ef747ba6eb
Mega <daxaka.msk@gmail.com>
2015-05-06 17:58:44 +0300

    Change all to better

--- clientside.js
+++ clientside.js
@@ -1,13 +1,14 @@
 /* ---------------------------------------------
-* nodebb-plugin-mega-realtime-online
-* Fix that force client-side NodeBB to ping server-side every 59 seconds
-* ---------------------------------------------*/
+ * nodebb-plugin-mega-realtime-online
+ * Fix that force client-side NodeBB to ping server-side every 59 seconds
+ * ---------------------------------------------*/
 
-$( document ).ready( function ( ) {
-	app.pingOnline = function ( ) {
-		socket.emit( 'modules.pingOnline', { }, function ( ) {
-			setTimeout( app.pingOnline, 3 * 1000 );
+$(document).ready(function() {
+	app.pingOnline = function() {
+		socket.emit('modules.pingOnline', {client: 'meow'}, function() {
+			console.log('socket.emit.pingOnline args: ', arguments);
+			setTimeout(app.pingOnline, 3 * 1000);
 		});
 	};
-	app.pingOnline( );
+	app.pingOnline();
 });
--- library.js
+++ library.js
@@ -1,43 +1,43 @@
 /* ---------------------------------------------
-* % Online status updater
-* While user ACTUALLY on the site he has status 'online less than one minute ago'
-* This module is the Pong answer for frontend (setInterval) Ping ( performs every 59 seconds )
-* ---------------------------------------------*/
-( function ( ) {
+ * % Online status updater
+ * While user ACTUALLY on the site he has status 'online less than one minute ago'
+ * This module is the Pong answer for frontend (setInterval) Ping ( performs every 59 seconds )
+ * ---------------------------------------------*/
+(function() {
 	'use strict';
 
-	var db = require.main.require( './src/database' ),
-		user = require.main.require( './src/user' ),
-		SocketModules = require.main.require( './src/socket.io/modules' );
+	var db = require.main.require('./src/database'),
+		user = require.main.require('./src/user'),
+		SocketModules = require.main.require('./src/socket.io/modules');
 
 	var Plugin = {
-		init: function ( params, callback ) {
+		init: function(params, callback) {
 
-			SocketModules.pingOnline = function ( socket, data, callback ) {
-				if ( !socket.uid ) return;
+			SocketModules.pingOnline = function(socket, data, callback) {
+				console.log('SocketModules.pingOnline', data, socket.uid);
+				if (!socket.uid) return;
 
-				( function( socket ) {
-					var callbackFix = function( ) { };
-					user.getUserFields( socket.uid, [ 'status', 'lastonline' ], function( err, userData ) {
-						var now = Date.now( );
-						if ( err || userData.status === 'offline' ) return callback( err );
+				var callbackFix = function() {},
+					now = Date.now();
 
-						user.setUserField( socket.uid, 'lastonline', now, callbackFix );
+				(function(socket) {
+					user.getUserFields(socket.uid, ['status', 'lastonline'], function(err, userData) {
+						if (err || userData.status === 'offline') return;
+
+						user.setUserField(socket.uid, 'lastonline', now, function(err) {
+							if (err) return;
+							db.sortedSetAdd('users:online', now, socket.uid, function(err) {
+								if (err) return;
+								callback('suka');
+							});
+						});
 					});
-				})( socket );
-
-				( function( socket ) {
-					var callbackFix = function( ) { };
-					var now = Date.now( );
-					db.sortedSetAdd( 'users:online', now, socket.uid, callbackFix );
-				})( socket );
-
-				callback( 'suka' );
+				})(socket);
 			};
 
-			callback( null );
+			callback(null);
 		}
 	};
 
 	module.exports = Plugin;
-})( );
+})();
