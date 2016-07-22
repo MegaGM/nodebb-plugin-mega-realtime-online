d81554cc406e2c1c2b71fab990e7bae1ae536903
Mega <Aza.Noriega@gmail.com>
2016-07-22 14:08:20 +0500

    rename to "app.ping", rewrite with Promises

--- clientside.js
+++ clientside.js
@@ -3,13 +3,13 @@
  * Fix that force client-side NodeBB to ping server-side every 59 seconds
  * ---------------------------------------------*/
 
-$(document).ready(function() {
-	app.pingOnline = function() {
-		socket.emit('plugins.pingOnline', {}, function(err, data) {
-			if (err) return console.log('nodebb-plugin-mega-realtime-online has been just stopped');
-			setTimeout(app.pingOnline, 5 * 1000);
-			console.log('pingOnline');
+$(document).ready(function () {
+	app.ping = function () {
+		socket.emit('plugins.ping', {}, function (err, data) {
+			if (err) return console.log(err);
+			// if (err) return console.log('realtime-online stopped', err);
+			setTimeout(app.ping, 2 * 1000);
 		});
 	};
-	app.pingOnline();
+	app.ping();
 });
--- library.js
+++ library.js
@@ -6,33 +6,26 @@
 (function () {
 	'use strict';
 
-	var db = require.main.require('./src/database'),
-		user = require.main.require('./src/user'),
-		SocketPlugins = require.main.require('./src/socket.io/plugins'),
-		SocketModules = require.main.require('./src/socket.io/modules');
+	let Promise = require('bluebird'),
+		db = Promise.promisifyAll(require.main.require('./src/database')),
+		user = Promise.promisifyAll(require.main.require('./src/user')),
+		SocketPlugins = require.main.require('./src/socket.io/plugins');
 
-	var Plugin = {
+	let Plugin = {
 		init: function (params, callback) {
-
-			SocketPlugins.pingOnline = function (socket, data, callback) {
+			SocketPlugins.ping = function (socket, data, callback) {
 				// TODO: debug
-				console.log('SocketPlugins.pingOnline', data, socket.uid);
-				if (!socket.uid) return;
+				// console.log('ping', data, socket.uid);
+				if (!socket.uid)
+					return callback(new Error('you\'re anonymous'));
 
-				var now = Date.now(),
-					callbackFix = function () {};
-
-				user.getUserFields(socket.uid, ['status', 'lastonline'], function (err, userData) {
-					if (err || userData.status === 'offline') return callback(true, 'an error occured');
-
-					user.setUserField(socket.uid, 'lastonline', now, function (err) {
-						if (err) return callback(true, 'an error occured');
-						db.sortedSetAdd('users:online', now, socket.uid, function (err) {
-							if (err) return callback(true, 'an error occured');
-							callback(null, 'no errors');
-						});
-					});
-				});
+				let now = Date.now();
+				// make user "online"
+				Promise.join(
+					user.setUserFieldAsync(socket.uid, 'lastonline', now),
+					db.sortedSetAddAsync('users:online', now, socket.uid),
+					() => callback(null)
+				);
 			};
 
 			// return control flow to NodeBB
